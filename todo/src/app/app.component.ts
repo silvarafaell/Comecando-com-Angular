667c207ea63a79c60dcd72483a7711faadcef2ce
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-05 08:07:55 -0300

    Objetos Complexos

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
 
   constructor() {
-    this.todos.push('passear com o cachorro');
-    this.todos.push('ir ao supermercado');
-    this.todos.push('cortar o cabelo');
+    this.todos.push(new Todo(1, 'Passear com o cachorro', true));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', false));
   }
 }

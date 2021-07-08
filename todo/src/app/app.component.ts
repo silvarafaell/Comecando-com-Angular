3b456e7499f8196c8255b668e19bb451686661eb
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-08 06:28:35 -0300

    Forms Basico

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,8 +10,16 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Passear com o cachorro', true));
     this.todos.push(new Todo(2, 'ir ao supermercado', true));
     this.todos.push(new Todo(3, 'cortar o cabelo', false));
